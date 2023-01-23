2f631c1c8857ab813379692bd0f58ed006f82d20
Bob Matoca <b0bmat0ca@gmail.com>
2023-01-23 21:44:10 +0900

    共通スクリプトのマージ調整

--- Assets/_MyWork/Scripts/PassthroughTarget.cs
+++ Assets/_MyWork/Scripts/PassthroughTarget.cs
@@ -3,52 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using RayFire;
 
-[RequireComponent(typeof(AudioSource))]
-public class Target : MonoBehaviour
+public class PassthroughTarget : Target
 {
-    [SerializeField] protected Collider colider;
-    [SerializeField] protected RayfireRigid rayFireRigid;
-    [SerializeField] protected float destroyTime = 5.0f;
-
-    protected MeshRenderer[] shatterMesh;   // RayFire Shutter で事前に粉砕されたオブジェクトの配列
-
     private Camera mainCamera;
-    private AudioSource audioSource;
 
-    protected virtual void Awake()
+    protected override void Awake()
     {
+        base.Awake();
         mainCamera = Camera.main;
-        audioSource = GetComponent<AudioSource>();
     }
 
     // Start is called before the first frame update
-    protected virtual void Start()
+    protected override void Start()
     {
-        shatterMesh = rayFireRigid.gameObject.GetComponentsInChildren<MeshRenderer>();
+        base.Start();
     }
 
     // Update is called once per frame
-    protected virtual void Update()
+    protected override void Update()
     {
+        base.Update();
         transform.LookAt(mainCamera.transform);
-    }
-
-    protected virtual void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Bullet"))
-        {
-            // 粉砕する
-            rayFireRigid.Demolish();
-
-            // コライダーを無効化する
-            colider.enabled = false;
-
-            // 粉砕後、{destroyTime}秒後にDestroyする
-            foreach (MeshRenderer mesh in shatterMesh)
-            {
-                Destroy(mesh.gameObject, destroyTime);
-            }
-            Destroy(gameObject, destroyTime);
-        }
     }
 }
