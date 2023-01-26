69c0b5c3710ff048252e0c77f9cb644e6ce37a60
Bob Matoca <b0bmat0ca@gmail.com>
2023-01-27 00:19:43 +0900

    共通アセット調整

--- Assets/_MyWork/Scripts/SpawnBoxTarget.cs
+++ Assets/_MyWork/Scripts/SpawnBoxTarget.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class SpawnBoxTarget : MonoBehaviour
 {
-    [SerializeField] private GameObject target;
-    [SerializeField] private float offsetY;
-    [SerializeField] private float targetScale;
+    // ターゲットリスト
+    [SerializeField] protected List<TargetMap> targetList;
+    [System.Serializable]
+    protected class TargetMap
+    {
+        [SerializeField] private GameObject target;
+        [SerializeField] private float offsetY;
+        [SerializeField] private float targetScale;
+
+        public GameObject Target { get { return target; } }
+        public float OffsetY { get { return offsetY; } }
+        public float TargetScale { get { return targetScale;} }
+    }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject obj =  Instantiate(target, transform.position + new Vector3(0, offsetY, 0), Quaternion.identity);
+        TargetMap target = targetList[Random.Range(0, targetList.Count)];
+        GameObject obj = Instantiate(target.Target, transform.position + new Vector3(0, target.OffsetY, 0), Quaternion.identity);
         GameObject child = obj.transform.GetChild(0).gameObject;
-        child.transform.localScale *= targetScale;
-        obj.GetComponent<BoxCollider>().size *= targetScale;
+        child.transform.localScale *= target.TargetScale;
+        //obj.GetComponent<BoxCollider>().size *= targetScale;
         child.SetActive(true);
     }
 
